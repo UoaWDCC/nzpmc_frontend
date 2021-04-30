5ca58cff2edd6152ed72e2790296100d56afaedc
matthew <matthewaptaylor@gmail.com>
2021-04-30 19:44:42 +1200

    Added theming colours

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#03a9f4',
+        secondary: '#009688',
+        accent: '#3f51b5',
+        error: '#f44336',
+        warning: '#ffeb3b',
+        info: '#00bcd4',
+        success: '#4caf50',
+      },
+    },
+  },  
 });
