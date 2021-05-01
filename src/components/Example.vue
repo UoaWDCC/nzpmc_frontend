4c250bc11d177c804cb62e309fe8b4a06338a00d
Will Zhang <yzhb120@aucklanduni.ac.nz>
2021-05-01 22:47:20 +1200

    Structral change

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    <h1>App Running!</h1>
-  </div>
+    <div>
+        <h1>App Running!</h1>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Example'
+    name: 'Example',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
