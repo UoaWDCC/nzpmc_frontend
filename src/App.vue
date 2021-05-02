c97fc655b17b260c9243e5a00a4802726a5dc289
matthew <matthewaptaylor@gmail.com>
2021-05-02 15:49:29 +1200

    Setup file structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Login</router-link> |
+      <router-link to="/welcome">Welcome</router-link> |
+      <router-link to="/exam">Exam</router-link> |
+      <router-link to="/finished">Finished</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login'
+import Welcome from '../views/Welcome'
+import Exam from '../views/Exam'
+import Finished from '../views/Finished'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/welcome',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/exam',
+    name: 'Exam',
+    component: Exam
+  },
+  {
+    path: '/finished',
+    name: 'Finished',
+    component: Finished
   }
 ]
 
