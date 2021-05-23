55596ebeb626b73d10e92f08f6e1f7ec63796cc4
dtor068 <dtor068@aucklanduni.ac.nz>
2021-05-23 14:50:46 +1200

    Started implementation of submission confirmation popup

---
+++ src/components/SubmissionConfirmation.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="submissionconfirmation">
+        <div align="center" class="hi">
+            <p>Are you sure you want to submit?</p>
+            <p>You will not be able to redo any questions after you submit.</p>
+        </div>
+        <v-row>
+            <v-btn depressed color="primary"> Primary </v-btn>
+        </v-row>
+    </div>
+</template>
