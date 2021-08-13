a2490d548db48cd742adc3cfd78578d304cd55a7
dtor068 <dtor068@aucklanduni.ac.nz>
2021-08-13 18:31:12 +1200

    Started implementing UserDisplay component

---
+++ src/components/UserDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-component v-if="searched === null">
+        <ol v-for="user in users" :key="user">
+            <v-card class="d-flex my-2 py-3 px-10 ml-4 mr-12">
+                {{ user }}
+                <v-btn icon small>
+                    <v-icon class="material-icons">edit</v-icon>
+                </v-btn>
+                <v-btn icon small>
+                    <v-icon class="material-icons">delete</v-icon>
+                </v-btn>
+            </v-card>
+        </ol>
+    </v-component>
+    <v-component v-else>
+        <v-card class="d-flex my-2 py-3 px-10 ml-4 mr-12">
+            {{ searched }}
+            <v-btn icon small>
+                <v-icon class="material-icons">edit</v-icon>
+            </v-btn>
+            <v-btn icon small>
+                <v-icon class="material-icons">delete</v-icon>
+            </v-btn>
+        </v-card>
+    </v-component>
+</template>
+
+<script>
+import { searchedUser } from './../components/UserSearchBar.vue'
+export default {
+    data() {
+        return {
+            users: ['Daniel', 'Will', 'CZ', 'Ruby', 'Matthew', 'Alex', 'Lance'],
+            searched: searchedUser,
+        }
+    },
+}
+</script>
--- src/views/UserAdmin.vue
+++ src/views/UserAdmin.vue
@@ -2,16 +2,19 @@
     <v-row>
         <v-col class="col-12">
             <UserSearchBar />
+            <UserDisplay />
         </v-col>
     </v-row>
 </template>
 
 <script>
 import UserSearchBar from './../components/UserSearchBar.vue'
+import UserDisplay from './../components/UserDisplay.vue'
 
 export default {
     components: {
         UserSearchBar,
+        UserDisplay,
     },
 }
 </script>
