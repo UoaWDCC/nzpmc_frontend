36d092409748efb8d893ee73bd2315a37649d42c
dtor068 <dtor068@aucklanduni.ac.nz>
2021-09-03 21:55:26 +1200

    Fixed up component

--- src/components/UserDisplay.vue
+++ src/components/UserDisplay.vue
@@ -6,12 +6,11 @@
         class="elevation-1"
     >
         <template v-slot:top>
-            <v-dialog v-model="dialog" max-width="500px">
+            <v-dialog v-model="dialogEdit" max-width="500px">
                 <v-card>
                     <v-card-title>
-                        <span class="text-h5">Edit Item</span>
+                        <span class="h5">Edit Item</span>
                     </v-card-title>
-
                     <v-card-text>
                         <v-container>
                             <v-row>
@@ -23,26 +22,25 @@
                                 </v-col>
                                 <v-col cols="12" sm="6" md="4">
                                     <v-text-field
-                                        v-model="editedItem.calories"
+                                        v-model="editedItem.email"
                                         label="Email"
                                     ></v-text-field>
                                 </v-col>
                                 <v-col cols="12" sm="6" md="4">
                                     <v-text-field
-                                        v-model="editedItem.fat"
+                                        v-model="editedItem.year"
                                         label="Year Level"
                                     ></v-text-field>
                                 </v-col>
                                 <v-col cols="12" sm="6" md="4">
                                     <v-text-field
-                                        v-model="editedItem.protein"
+                                        v-model="editedItem.quizzes"
                                         label="Associated Quiz(s)"
                                     ></v-text-field>
                                 </v-col>
                             </v-row>
                         </v-container>
                     </v-card-text>
-
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn color="blue darken-1" text @click="close">
@@ -52,30 +50,30 @@
                             Save
                         </v-btn>
                     </v-card-actions>
-                </v-card> </v-dialog
-            ><v-dialog v-model="dialogDelete" max-width="500px">
+                </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
                 <v-card>
-                    <v-card-title class="text-h5"
-                        >Are you sure you want to delete this
-                        item?</v-card-title
-                    >
+                    <v-card-title class="=h5" align="centre">
+                        Delete this item?
+                    </v-card-title>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="closeDelete"
-                            >Cancel</v-btn
-                        >
+                        <v-btn color="blue darken-1" text @click="closeDelete">
+                            Cancel
+                        </v-btn>
                         <v-btn
                             color="blue darken-1"
                             text
                             @click="deleteItemConfirm"
-                            >OK</v-btn
                         >
-                        <v-spacer></v-spacer>
+                            OK
+                        </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
         </template>
-        <template v-slot:item.actions="{ item }">
+        <template v-slot:[`item.actions`]="{ item }">
             <v-icon small class="mr-2 material-icons" @click="editItem(item)">
                 edit
             </v-icon>
@@ -90,13 +88,12 @@
 <script>
 export default {
     data: () => ({
-        dialog: false,
+        dialogEdit: false,
         dialogDelete: false,
         headers: [
             {
                 text: 'Name',
                 align: 'start',
-                sortable: false,
                 value: 'name',
             },
             { text: 'Email', value: 'email' },
@@ -104,26 +101,43 @@ export default {
             { text: 'Associated Quiz(s)', value: 'quizzes' },
             { text: 'Actions', value: 'actions', sortable: false },
         ],
-        users: [],
+        users: [
+            {
+                name: 'Daniel',
+                email: 'daniel@gmail.com',
+                year: 6,
+                quizzes: 3,
+            },
+            {
+                name: 'Will',
+                email: 'will@gmail.com',
+                year: 8,
+                quizzes: 24,
+            },
+            {
+                name: 'CZ',
+                email: 'cz@gmail.com',
+                year: 12,
+                quizzes: 6,
+            },
+        ],
         editedIndex: -1,
         editedItem: {
             name: '',
-            calories: 0,
-            fat: 0,
-            carbs: 0,
-            protein: 0,
+            email: '',
+            year: 0,
+            quizzes: 0,
         },
         defaultItem: {
             name: '',
-            calories: 0,
-            fat: 0,
-            carbs: 0,
-            protein: 0,
+            email: '',
+            year: 0,
+            quizzes: 0,
         },
     }),
 
     watch: {
-        dialog(val) {
+        dialogEdit(val) {
             val || this.close()
         },
         dialogDelete(val) {
@@ -136,48 +150,25 @@ export default {
     },
 
     methods: {
-        initialize() {
-            this.users = [
-                {
-                    name: 'Daniel',
-                    email: 'daniel@gmail.com',
-                    year: 6.0,
-                    quizzes: 3,
-                },
-                {
-                    name: 'Will',
-                    email: 'will@gmail.com',
-                    year: 8,
-                    quizzes: 24,
-                },
-                {
-                    name: 'CZ',
-                    email: 'cz@gmail.com',
-                    year: 12,
-                    quizzes: 6,
-                },
-            ]
-        },
-
         editItem(item) {
-            this.editedIndex = this.desserts.indexOf(item)
+            this.editedIndex = this.users.indexOf(item)
             this.editedItem = Object.assign({}, item)
-            this.dialog = true
+            this.dialogEdit = true
         },
 
         deleteItem(item) {
-            this.editedIndex = this.desserts.indexOf(item)
+            this.editedIndex = this.users.indexOf(item)
             this.editedItem = Object.assign({}, item)
             this.dialogDelete = true
         },
 
         deleteItemConfirm() {
-            this.desserts.splice(this.editedIndex, 1)
+            this.users.splice(this.editedIndex, 1)
             this.closeDelete()
         },
 
-        close() {
-            this.dialog = false
+        closeEdit() {
+            this.dialogEdit = false
             this.$nextTick(() => {
                 this.editedItem = Object.assign({}, this.defaultItem)
                 this.editedIndex = -1
@@ -194,11 +185,11 @@ export default {
 
         save() {
             if (this.editedIndex > -1) {
-                Object.assign(this.desserts[this.editedIndex], this.editedItem)
+                Object.assign(this.users[this.editedIndex], this.editedItem)
             } else {
-                this.desserts.push(this.editedItem)
+                this.users.push(this.editedItem)
             }
-            this.close()
+            this.closeEdit()
         },
     },
 }
